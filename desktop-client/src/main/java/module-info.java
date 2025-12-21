2954d5e31c63e1aafa5de4a052fe9b26805cf57e
Andrew Muhlynin <rx.081@hotmail.com>
2025-12-21 21:48:55 +0500

    Осваиваю Java-FX

--- desktop-client/src/main/java/module-info.java
+++ desktop-client/src/main/java/module-info.java
@@ -1,16 +1,17 @@
-module task.manager.desktopclient {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires javafx.web;
+module project.task.manager.desktopclient {
+		requires javafx.controls;
+		requires javafx.fxml;
+		requires javafx.web;
 
-    requires org.controlsfx.controls;
-    requires com.dlsc.formsfx;
-    requires net.synedra.validatorfx;
-    requires org.kordamp.ikonli.javafx;
-    requires org.kordamp.bootstrapfx.core;
-    requires eu.hansolo.tilesfx;
-    requires com.almasb.fxgl.all;
+		requires org.controlsfx.controls;
+		requires com.dlsc.formsfx;
+		requires net.synedra.validatorfx;
+		requires org.kordamp.ikonli.javafx;
+		requires org.kordamp.bootstrapfx.core;
+		requires eu.hansolo.tilesfx;
+		requires com.almasb.fxgl.all;
+		requires kotlin.stdlib;
 
-    opens task.manager.desktopclient to javafx.fxml;
-    exports task.manager.desktopclient;
+		opens project.task.manager.desktopclient to javafx.fxml;
+		exports project.task.manager.desktopclient;
 }
